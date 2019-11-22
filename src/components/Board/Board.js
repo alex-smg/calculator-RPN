5a740666aac09c517073e434d796cf9a78061c39
Vincent Schlosser <s.vincent@live.fr>
2019-11-22 10:27:49 +0100

    Clean and add first structure

---
+++ src/components/Board/Board.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Board.css';
+import Button from "../Button/Button";
+
+function Board() {
+    return (
+        <div>
+        </div>
+    );
+}
+
+export default Board;
