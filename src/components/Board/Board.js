23adc7a8942db7a8ad67fe13a161f3909587dd9b
Alexandre Smagghe <alex.s95120@gmail.com>
2019-11-28 23:52:40 +0100

    add delete on scribe

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -16,8 +16,8 @@ class Board extends React.Component {
 
         let fakePrepile = this.state.prePile;
 
-        let prePileJoin = `${fakePrepile}${number}`
-        let result = parseInt(prePileJoin, 10)
+        let prePileJoin = `${fakePrepile}${number}`;
+        let result = parseInt(prePileJoin, 10);
 
         this.setState((state, props) => ({
             prePile: result,
@@ -25,37 +25,37 @@ class Board extends React.Component {
             console.log(this.state)
           })
         // TODO: Add multiple "number" to do ONE value
-    }
+    };
 
     calculAction =  (action) => {
-        let result
+        let result;
 
-        const pile = this.state.pile
-        const pileLength = pile.length
+        const pile = this.state.pile;
+        const pileLength = pile.length;
 
         if (pileLength >= 2) {
 
-            const a = pile.pop()
-            const b = pile.pop()
+            const a = pile.pop();
+            const b = pile.pop();
 
             switch (action) {
                 case '+':
-                    result = b + a
-                    break
+                    result = b + a;
+                    break;
                 case '-':
-                    result = b - a
-                    break
+                    result = b - a;
+                    break;
                 case '*':
-                    result = b * a
-                    break
+                    result = b * a;
+                    break;
                 case '/':
-                    result = b / a
-                    break
+                    result = b / a;
+                    break;
                 default:
-                    break
+                    break;
             }
 
-            pile.push(result)
+            pile.push(result);
 
             this.setState( (state, props) => ({
                     pile: pile
@@ -109,22 +109,33 @@ class Board extends React.Component {
             )
         }
     }
+    deleteItemAction() {
+        const prepile = this.state.prePile.toString();
+        const prePileDelete = prepile.substring(0,prepile.length-1);
+        const result = parseInt(prePileDelete, 10);
+        console.log(prePileDelete);
+        this.setState((state, props) => ({
+            prePile: result,
+        }), () => {
+            console.log(this.state.prePile);
+        })
+    }
 
         render() {
-            const numbers = [0, 1, 2, 3, 4, 5, 6 ,7, 8, 9]
-            const actions = ['-', '+', '*', '/']
+            const numbers = [9,8,7,6,5,4,3,2,1,0];
+            const actions = ['-', '+', '*', '/'];
 
             return (
-                <div>
+                <div className="calculatrice">
                     <Screen affiche={this.state.prePile} affichePile={this.state.pile}></Screen>
                     <div className="clavier">
                      { numbers.map((number, index) => (<Button key={index} handleClick={() => this.addNumber(number)} value={number} /> )) }
+                        { actions.map((action, index) => <Button handleClick={() => this.calculAction(action)} key={index} value={action} />) }
+                        <Button handleClick={() => this.enterAction()} value='enter' />
+                        <Button handleClick={() => this.dropItemAction()} value='drop' />
+                        <Button handleClick={() => this.swapItemAction()} value='swap' />
+                        <Button handleClick={() => this.deleteItemAction()} value='<-' />
                     </div>
-                    
-                    { actions.map((action, index) => <Button handleClick={() => this.calculAction(action)} key={index} value={action} />) }
-                    <Button handleClick={() => this.enterAction()} value='enter' />
-                    <Button handleClick={() => this.dropItemAction()} value='drop' />
-                    <Button handleClick={() => this.swapItemAction()} value='swap' />
                 </div>
             );
         }
--- src/components/screen/Screen.js
+++ src/components/screen/Screen.js
@@ -5,7 +5,9 @@ function Screen(props) {
     return (
         <div className="ecran">
             <h3>{ props.affiche }</h3>
-            <h4>{ props.affichePile}</h4>
+            <ul>{ props.affichePile.map((person, index) => (
+                <li>{person}</li>
+            ))}</ul>
         </div>
     );
 }
